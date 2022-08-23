8729269c80f0d5f782cfe5c748292c2e3dc378d0
Oleksii Shyrokov <oshyrokov@zuora.com>
2022-08-23 17:12:39 +0300

    Fix perm6

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ export ACTION_PATH=${1}
 export DATA_FROM_INPUT=${2}
 echo $DATA_FROM_INPUT
 
+echo $GH_DATA_NEW | base64 -d
 
 export DATA_TO_SAVE=$(echo $DATA_FROM_INPUT | base64 -d)
 export PATH_TO_SAVE=$(echo $ACTION_PATH | base64 -d)
