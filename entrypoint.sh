43f46e06c487b1c3eb2c7005d3138e35df7dff1a
Oleksii Shyrokov <oshyrokov@zuora.com>
2022-08-23 16:59:54 +0300

    Fix perm4

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@
 cd $GITHUB_WORKSPACE
 
 pwd
+env
 
 export ACTION_PATH=${1}
 export DATA_FROM_INPUT=${2}
+echo $DATA_FROM_INPUT
 
-DATA_TO_SAVE=$(echo $DATA_FROM_INPUT | base64 -d)
-PATH_TO_SAVE=$(echo $ACTION_PATH | base64 -d)
+DATA_TO_SAVE=$(echo $DATA_FROM_INPUT | base64 -di)
+PATH_TO_SAVE=$(echo $ACTION_PATH | base64 -di)
 # Deploy to GitHub Pages
 
 echo $DATA_TO_SAVE
