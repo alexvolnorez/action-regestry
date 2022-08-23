06aaff888903236a8794bdfc20777db2869d88b1
Oleksii Shyrokov <oshyrokov@zuora.com>
2022-08-23 17:09:24 +0300

    Fix perm5

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,9 @@ export ACTION_PATH=${1}
 export DATA_FROM_INPUT=${2}
 echo $DATA_FROM_INPUT
 
-DATA_TO_SAVE=$(echo $DATA_FROM_INPUT | base64 -di)
-PATH_TO_SAVE=$(echo $ACTION_PATH | base64 -di)
+
+export DATA_TO_SAVE=$(echo $DATA_FROM_INPUT | base64 -d)
+export PATH_TO_SAVE=$(echo $ACTION_PATH | base64 -d)
 # Deploy to GitHub Pages
 
 echo $DATA_TO_SAVE
