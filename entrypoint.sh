69f951ec47053640047fc7cbd61d597fa4adc406
Oleksii Shyrokov <oshyrokov@zuora.com>
2022-08-24 09:48:34 +0300

    add new env

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ cd $GITHUB_WORKSPACE
 pwd
 env
 
-export ACTION_PATH=${1}
-export DATA_FROM_INPUT=${2}
-echo $DATA_FROM_INPUT
+#export ACTION_PATH=${1}
+#export DATA_FROM_INPUT=${2}
+#echo $DATA_FROM_INPUT
 
-echo $GH_DATA_NEW | base64 -d
+#echo $GH_DATA_NEW | base64 -d
 
-export DATA_TO_SAVE=$(echo $DATA_FROM_INPUT | base64 -d)
-export PATH_TO_SAVE=$(echo $ACTION_PATH | base64 -d)
+export DATA_TO_SAVE=$(echo $GH_DATA_NEW | base64 -d)
+export PATH_TO_SAVE=$(echo $GH_PATH_NEW | base64 -d)
 # Deploy to GitHub Pages
 
 echo $DATA_TO_SAVE
